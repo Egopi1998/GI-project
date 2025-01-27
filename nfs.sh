46a8c1f90e9011b6ad39443005682f24baddb403
E.Gopi <egopimani1998@gmail.com>
2025-01-27 19:47:56 +0530

    firts

--- nfs.sh
+++ nfs.sh
@@ -4,8 +4,24 @@
 
 NFS_SERVER_1="root@10.188.47.238"
 NFS_SERVER_1_PASSWORD="bMccW2NgBLBw"
-BACKUP_DIR=/data/insights/
+BACKUP_DIR_MAIN="/data/insights/"
+BACKUP_DIR="/data/insights/3.6.0-backup/3.6.0-ga"
+TIMESTAMP=$(date +%F-%H-%M)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
-cd $BACKUP_DIR
+# changing to the main directory
+
+if [ "$PWD" = "${BACKUP_DIR}" ]; then
+    echo "OK"
+else
+    cd $BACKUP_DIR_MAIN && 
+    mkdir $BACKUP_DIR &&
+    chmod 777 -R BACKUP_DIR 
+    echo "Navigate to ${BACKUP_DIR_MAIN}"
+fi
+
+
+
 
 
