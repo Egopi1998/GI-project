f00415d4889c2f6af449c42ba9089b0aeed8bfbc
E.Gopi <egopimani1998@gmail.com>
2025-01-27 19:24:49 +0530

    first commit

---
+++ backup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#making cluster as variable 
+CLUSTER="oc login -u kubeadmin -p Q8a7j-AdEhS-nXd3b-896Wn --server=https://api.komal-bnr.cp.fyre.ibm.com:6443"
---
+++ nfs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# we need configure NFS server as node, so below commands will run in nfs server 
+
+NFS_SERVER_1="root@10.188.47.238"
+NFS_SERVER_1_PASSWORD="bMccW2NgBLBw"
+BACKUP_DIR=/data/insights/
+
+cd $BACKUP_DIR
+
+
